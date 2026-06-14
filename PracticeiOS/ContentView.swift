24ecc71af85ac53376f01e5c3c59faf3d0ff66b2
Sahi Joshi <sahijoshi@Sahis-MacBook-Air.local>
2026-06-14 15:03:26 +0545

    Initial project

---
+++ PracticeiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeiOS
+//
+//  Created by Sahi Joshi on 14/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeiOS/PracticeiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeiOSApp.swift
+//  PracticeiOS
+//
+//  Created by Sahi Joshi on 14/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PracticeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
