626839d30b8a3ac9d4d34bb314fbb346f5d9c6d1
Sahi Joshi <sahijoshi@Sahis-MacBook-Air.local>
2026-06-15 17:30:26 +0545

    Add welcome screen

--- PracticeiOS/WelcomeView.swift
+++ PracticeiOS/WelcomeView.swift
@@ -17,6 +17,47 @@ struct WelcomeView: View {
                 .foregroundColor(.black)
                 .opacity(0.5)
             
+            VStack {
+                VStack(spacing:10) {
+                    Text("Find Your Perfect Venue")
+                        .foregroundColor(.white)
+                        .font(.system(size: 36, weight: .medium, design: .serif))
+                        .multilineTextAlignment(.center)
+                    
+                    Text("Discover banquet halls, cafés & event spaces across Kathmandu")
+                        .foregroundColor(.white)
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                }
+                .padding(.horizontal, 60)
+                .padding(.bottom, 60)
+
+                Button {
+                    
+                } label: {
+                    Text("GET STARTED")
+                }
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, maxHeight: 52)
+                .background(.orange)
+                .cornerRadius(14)
+                
+                HStack {
+                    Text("Already have an account?")
+                        .foregroundColor(.white)
+                        
+                    Button {
+                        
+                    } label: {
+                        Text("Sign In")
+                    }
+                    .foregroundStyle(.white)
+                    .underline()
+                    .font(.headline)
+                }
+                .padding(.top, 20)
+            }
+            .padding(.horizontal, 10)
         }
         .ignoresSafeArea()
     }
